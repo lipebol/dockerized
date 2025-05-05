6bc571708e26c7c322d110d250114a39916ebe01
lipebol <lipe_bol5@hotmail.com>
2025-05-05 19:40:17 -0300

    add new http request mode and exception handling

--- air-flowEx/local/dags/flows/spotifEx/src/web.py
+++ air-flowEx/local/dags/flows/spotifEx/src/web.py
@@ -4,49 +4,46 @@ from sys import path
 path.insert(0, abspath(join(dirname(__file__), '..', '..', '..')))
 
 from ast import literal_eval
-from inspect import currentframe
 from os import getenv
-from utEx.fetch import Fetch
+from utEx.httpEx import HttpEx
+from system import System
 
 
 class Web:
 
-    def __init__(self, System, metadata: dict) -> None:
-        self._System, self.__metadata, self._Fetch = System, metadata, Fetch()
+    def __init__(self, metadata: dict) -> None:
+        self.__metadata = metadata
     
+    @System.exceptions
     def info(self) -> dict or bool:
-        try:
-            if self.__metadata:
-                if self.__metadata['title'] != getenv('AD'):
-                    self._artist = "".join(self.__metadata['artist'])
-                    if getenv('SINGLE_QUOTE') in self._artist: ### Example: Joseph O'Brien
-                        self.idx = self._artist.find(getenv('SINGLE_QUOTE'))
-                        ### 'r' (raw string) for 'escape' (Joseph O\'Brien)
-                        self._artist = "".join(
-                            fr"{self._artist[:self.idx]}\{self._artist[self.idx:]}"
-                        )
-                    self.__soup = self._Fetch.scrape(
-                        getenv('EVERY_NOISE') + 'lookup.cgi', 'a',
-                        literal_eval(getenv('PARAMS') % self._artist)
+        if self.__metadata:
+            if self.__metadata['title'] != getenv('AD'):
+                self._artist = "".join(self.__metadata['artist'])
+                if getenv('SINGLE_QUOTE') in self._artist: ### Example: Joseph O'Brien
+                    self.idx = self._artist.find(getenv('SINGLE_QUOTE'))
+                    ### 'r' (raw string) for 'escape' (Joseph O\'Brien)
+                    self._artist = "".join(
+                        fr"{self._artist[:self.idx]}\{self._artist[self.idx:]}"
                     )
-                    return {
-                        "listen": 1, 
-                        "track": self._mergeInfo(
-                            [
-                                {
-                                    (
-                                        i.text if str(i.text) != "â\x93\x98" else ""
-                                    ) : getenv('EVERY_NOISE') + self.__soup[idx]['href']
-                                } for idx, i in enumerate(self.__soup)
-                            ]
-                        )
-                    }
-            return False
-        except Exception as error:
-            self._System.notify(f"{error} in: @{currentframe().f_code.co_name}")
-            return False
+                self.__soup = HttpEx.scrape(
+                    getenv('EVERY_NOISE') + 'lookup.cgi', tag='a',
+                    params=literal_eval(getenv('PARAMS') % self._artist)
+                )
+                return {
+                    "listen": 1, 
+                    "track": self._mergeInfo(
+                        [
+                            {
+                                (
+                                    i.text if str(i.text) != "â\x93\x98" else ""
+                                ) : getenv('EVERY_NOISE') + self.__soup[idx]['href']
+                            } for idx, i in enumerate(self.__soup)
+                        ]
+                    )
+                }
+        return False
 
-
+    @System.exceptions
     def _mergeInfo(self, info: list) -> dict:
         for dictio in info:
             if "".join(dictio.keys()) == "":
@@ -58,13 +55,9 @@ class Web:
         self.__metadata.pop('albumArtist')
         return self.__metadata
 
-
+    @System.exceptions
     def image(self) -> bytes or bool:
-        try:
-            return self._Fetch.content(self.__metadata['artUrl'])
-        except Exception as error:
-            self._System.notify(f"{error} in: @{currentframe().f_code.co_name}")
-            return False
+        return HttpEx.content(self.__metadata['artUrl'])
 
 
     
