9e1090b5187c55a3f096cc86843aea013f0c308c
lipebol <lipe_bol5@hotmail.com>
2025-05-17 20:48:23 -0300

    rm comment

--- webservEx/src/rest/routes/r-jobs.js
+++ webservEx/src/rest/routes/r-jobs.js
@@ -2,11 +2,8 @@ const router = require(process.env.EXPRESS).Router()
 const { c_ } = require(process.env.CONTROLLER_PATH)
 
 
-
-/// 'jobs.website'
 router.get(process.env.JOBS_WEBSITE_ENDPOINT, c_.noParams)
 
-/// 'jobs.search'
 router.get(process.env.JOBS_KEYWORD_ENDPOINT, c_.noParams)
 
 
