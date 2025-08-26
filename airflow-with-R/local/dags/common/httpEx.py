b5f7b6a030db9eddcb96ea7cbc2750eaf861290b
lipebol <lipe_bol5@hotmail.com>
2025-08-26 01:12:00 -0300

    rewritten

--- airflow-with-R/local/dags/common/httpEx.py
+++ airflow-with-R/local/dags/common/httpEx.py
@@ -1,43 +1,39 @@
-from ast import literal_eval
-from bs4 import BeautifulSoup
-from inspect import currentframe
-from os import getenv
+from common.initialize import add, load
+# from bs4 import BeautifulSoup
 from requests import get
 
 
-class HttpExError(Exception):
-    pass
-
-class HttpEx:
+class httpEx:
     
+    @add.exception
     @staticmethod
-    def httpEx(func):
+    def __getdata(func):
         def wrapper(*args, **kwargs):
             request = get(
-                "".join(args), params=kwargs.get('params'),
-                headers=literal_eval(getenv('HEADERS')), timeout=2
+                kwargs.get('url') if kwargs.get('url') else "".join(args), 
+                params=kwargs.get('params'),
+                headers=load.variable('HEADERS', eval=True), timeout=2
             )
             if request.status_code == 200:
-                kwargs['response'] = request.text if func.__name__ in (
-                    'scrape') else request.content ### <-- add names methods...
+                kwargs['response'] = request.text if kwargs.get('text') else request.content
                 return func(*args, **kwargs)
-            return HttpExError('Error in request.')
+            return Exception('Error in request.')
         return wrapper
 
-    @httpEx
-    @staticmethod
-    def scrape(url: str, **kwargs) -> str:
-        try:
-            return BeautifulSoup(kwargs.get('response'),'lxml').find_all(kwargs.get('tag'))
-        except Exception as error:
-            print(f"{error} in: @{currentframe().f_code.co_name}")
-            raise HttpExError
+    # @add.exception
+    # @__getdata
+    # @staticmethod
+    # def scrape(**kwargs, text=True) -> str:
+    #     return BeautifulSoup(kwargs.get('response'),'lxml').find_all(kwargs.get('tag'))
+        
 
-    @httpEx
+    @add.exception
+    @__getdata
     @staticmethod
-    def content(url: str, **kwargs) -> bytes:
-        try:
-            return kwargs.get('response')
-        except Exception as error:
-            print(f"{error} in: @{currentframe().f_code.co_name}")
-            raise HttpExError
+    def save(**kwargs):
+        if not kwargs.get('filename'):
+            kwargs['filename'] = kwargs.get('url').split('/')[-1]
+        open(
+            kwargs.get('flowpath') + '/datasets/' + kwargs.get('filename'), 'wb'
+        ).write(kwargs.get('response'))
+        return load.variable('MESSAGE_SUCCESS')
