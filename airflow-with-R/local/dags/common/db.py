681d7421a1a6419c966009fa2211c2281910957d
lipebol <lipe_bol5@hotmail.com>
2025-08-28 00:56:14 -0300

    add 'exception' and line in method: 'select'

--- airflow-with-R/local/dags/common/db.py
+++ airflow-with-R/local/dags/common/db.py
@@ -7,21 +7,28 @@ class db:
     @property
     def __postgres(self):
         return PostgresHook(load.variable('POSTGRES_CONN'))
- 
+    
+    @add.exception
     @staticmethod
     def conn():
         return db().__postgres.get_conn()
     
+    @add.exception
     @staticmethod
-    def select(sql_command: str):
+    def select(schema: str, table: str, params=None):
+        sql_command = load.variable('SELECT_COMMAND') % (schema, table)
+        if params:
+            pass
         return db().__postgres.get_records(sql_command)
 
     @add.exception
-    def adbc(pyarrowTable: object, table: str, schema: str) -> str:
+    @staticmethod
+    def adbc(pyarrowTable: object, schema: str, table: str) -> str:
         __conn = adbc_driver_postgresql.dbapi.connect(db().__postgres.get_uri())
         with __conn.cursor() as cursor:
             cursor.adbc_ingest(
-                db_schema_name=schema, table_name=table, data=pyarrowTable, mode='append'
+                db_schema_name=schema, table_name=table, 
+                data=pyarrowTable, mode='append'
             )
         __conn.commit()
         __conn.close()
