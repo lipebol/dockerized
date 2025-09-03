b5f2f609fc3d43d53f72f824fab94d4130ebe8cd
lipebol <lipe_bol5@hotmail.com>
2025-09-02 23:47:16 -0300

    rewritten

--- airflow-with-R/local/dags/flows/divvy_bikes/python/processing.py
+++ airflow-with-R/local/dags/flows/divvy_bikes/python/processing.py
@@ -7,27 +7,30 @@ import pyarrow as arrow
 
 class processing:
 
-    flowconf.cols = db.columns(flowconf.schema, flowconf.table[1])
-
-    @staticmethod
-    def __data(csv_file: str):
-        return file.load_csv(
-            csv_file, flowconf.cols, dict(
-                (col,arrow.string()) if col not in ('started_at','ended_at') 
-                else (col,arrow.timestamp('ms')) for col in flowconf.cols
-            ), sep=','
-        )
 
     @staticmethod
     def unpack():
-        for zip_file in load().zip_files:
+        __zip_files = load().zip_files()
+        for zip_file in __zip_files:
             load.unzip(load.path(zip_file), suffix='.csv')
-        if len(load().csv_files) < len(load().zip_files):
+        if len(load().csv_files) < len(__zip_files):
             raise Exception('')
         return load.variable('MESSAGE_SUCCESS')
 
     @staticmethod
-    def load():
-        for csv_file in load().csv_files:
-            db.adbc(processing.data(csv_file), flowconf.schema, flowconf.table[1])
+    def tripdata_gte_2020():
+        __cols = db.columns(flowconf.schema, 'tripdata_gte_2020')
+        __types = dict(
+            (col,arrow.string()) if col not in ('started_at','ended_at') 
+            else (col,arrow.timestamp('ms')) for col in __cols
+        )
+        __csv_files = sorted([
+            csv_file for csv_file in load().csv_files 
+            if 'tripdata' in csv_file or 
+            load.path(csv_file).name == 'Divvy_Trips_2020_Q1.csv'
+        ])
+        for csv_file in __csv_files:
+            __data = file.load_csv(csv_file, __cols, __types, sep=',')
+            __todb = db.adbc(__data, flowconf.schema, 'tripdata_gte_2020')
+            print(f'{load.path(csv_file).name}[{__data.num_rows} rows] ({__todb})')
         return load.variable('MESSAGE_SUCCESS')
