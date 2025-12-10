d1976d7d385f9d72f46b77e39d8475be0ee0eac2
lipebol <lipe_bol5@hotmail.com>
2025-12-10 21:16:11 +0000

    moved dir, renamed and rewritten

---
+++ express-graphql/src/common/controllers.mjs
@@ -0,0 +1,51 @@
+import { Service } from './service.js'
+import { GetHandler } from './handlers/request-response/get.js'
+
+export class Controllers {
+
+    static async dates(request, response) {
+        try {
+            const set = new Set(request, response)
+            request = set.Request()
+            console.log(request)
+            // console.log(new HasParams().Dates(request))
+            console.log(new ThisParams().isDates(request))
+            // if (new HasParams().Dates(request)) {
+            //     const validation = await new ThisParams().isDates(request)
+            //     if (typeof (validation) === process.env.TYPE_OBJECT) {
+            //         const content = await set.Who(validation)
+            //         return set.Response(content ? content : { NotFound: process.env.NOT_FOUND })
+            //     }
+            //     return set.Response({ BadRequest: process.env[validation] })
+            // }
+            // return set.Response({ BadRequest: process.env.REQUIRED_FIELDS })
+        } catch (err) { return set.Response({ InternalServerError: err }) }
+    }
+
+
+    static async multi(request, response) {
+        const handler = await GetHandler.create(request, response)
+        if (handler.params && !handler.error) {
+            if (!handler.data) {
+                handler.data = await Service[handler.db](handler)
+            }
+            return GetHandler.response(handler)
+        }
+        return this.noparams(handler, response)
+    }
+
+
+    static async noparams(handler, response) {
+        try {
+            if (!handler.error) {
+                handler = handler.model && handler.db ?
+                    handler : await GetHandler.create(handler, response)
+                handler.data = await Service[handler.db](handler)
+            }
+            return GetHandler.response(handler)
+        } catch (error) {
+            console.log(error)
+            GetHandler.response(response, { InternalServerError: err })
+        }
+    }
+}
