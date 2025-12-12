1cad7e5acd46846617818b9fbe40bb074fa1c404
lipebol <lipe_bol5@hotmail.com>
2025-12-12 17:38:42 +0000

    rewritten

--- express-graphql/src/common/handlers/request-response/externals.mjs
+++ express-graphql/src/common/handlers/request-response/externals.mjs
@@ -8,72 +8,129 @@ export class Externals {
     }
 
     static async spotifyAPI(handler) {
-        let response = await Externals.get(
-            `https://api.spotify.com/v1/tracks/${handler.params}`,
-            handler.authExternal
+
+        const [graphql, query, spotify] = [
+            'http://localhost:3000/api/v2/graphql',
+            (type, filter, id) => {
+                return {
+                    method: 'POST', body: {
+                        query: `query { 
+                    ${type}(${filter}: \"${id}\", lookup: false) 
+                    { ...on ${type} { data { ${filter} } } 
+                    ...on Errors { error message status_code } }}`
+                    }
+                }
+            }, `https://api.spotify.com/v1/tracks/${handler.params}`
+        ]
+
+        let track = await Externals.get(
+            graphql, query(handler.about.type, handler.filter, handler.params)
         )
 
-        handler[response.status_code ? 'error' : 'data'] = response.status_code ?
-            response : {
-                duration_ms: response.duration_ms, disc_number: response.disc_number,
-                track_number: response.track_number, popularity: response.popularity,
-                explicit: response.explicit, isrc: response.external_ids.isrc,
-                url: response.external_urls.spotify, name: response.name
-            }
+        if (!track.data[handler.about.type]?.data) {
 
+            track = await Externals.get(spotify, { token: handler.authExternal })
 
-        if (!handler.error && handler.fields.includes('album')) {
-            response = await Externals.get(
-                response.album.href, handler.authExternal
-            )
-
-            if (response.status_code) { handler.error = response }
-            else {
-                handler.data.album = {
-                    id: response.id, name: response.name, album_type: response.album_type,
-                    total_tracks: response.total_tracks, copyrights: response.copyrights,
-                    external_url: response.external_urls.spotify, label: response.label,
-                    release_date: response.release_date, images: response.images,
-                    available_markets: response.available_markets
+            handler.data = track.status_code ?
+                { error: track } : {
+                    url: track.external_urls?.spotify, name: track.name, trackid: track.id,
+                    duration_ms: track.duration_ms, disc_number: track.disc_number,
+                    track_number: track.track_number, popularity: track.popularity,
+                    explicit: track.explicit, isrc: track.external_ids?.isrc,
                 }
-            }
-        }
 
-        if (!handler.error && handler.fields.includes('artists')) {
-            response = await Promise.all(
-                response.artists.map(
-                    async (artist) => {
-                        const response = await Externals.get(
-                            artist.href, handler.authExternal
-                        )
-                        return {
-                            id: response.id, name: response.name,
-                            profile: response.external_urls.spotify,
-                            followers: response.followers.total,
-                            images: response.images, genres: response.genres
-                                .map((genre) => { return { name: genre, about: '' } })
+            if (!handler.data?.error && handler.fields.includes('album')) {
+                let album = await Externals.get(
+                    graphql, query('spotifExAlbums', 'albumid', track.album?.id)
+                )
+
+                if (!album.data['spotifExAlbums']?.data) {
+                    album = await Externals.get(
+                        track.album?.href, { token: handler.authExternal }
+                    )
+
+                    if (album.status_code) {
+                        handler.data = { error: album }
+                    } else {
+                        handler.data.album = {
+                            albumid: album.id, name: album.name, label: album.label,
+                            release_date: album.release_date, images: album.images,
+                            available_markets: album.available_markets,
+                            external_url: album.external_urls?.spotify,
+                            total_tracks: album.total_tracks,
+                            album_type: album.album_type,
+                            copyrights: album.copyrights
                         }
                     }
-                )
-            )
-            if (response.status_code) { handler.error = response }
-            else { handler.data.artists = response }
+                }
+            }
+
+
+            if (!handler.data?.error && handler.fields.includes('artists')) {
+                let ids = track.artists?.map(artist => artist.id)
+                let artists = await Externals.get(
+                    graphql, query('spotifExArtists', 'artistid', ids.join('|'))
+                ).then(artists => artists.data['spotifExArtists'])
+
+                artists = artists?.error ? track.artists?.map(artist => artist.href) :
+                    (() => {
+                        ids = ids.filter(
+                            artist => !new Set(
+                                artists?.data?.map(artist => artist.artistid)
+                            ).has(artist)
+                        )
+                        return ids.length === 0 ? null :
+                            track.artists?.filter(
+                                artist => new Set(ids).has(artist.id)
+                            )?.map(artist => artist.href)
+                    })()
+
+                if (artists) {
+                    artists = await Promise.all(
+                        artists.map(
+                            async (artist) => {
+                                artist = await Externals.get(
+                                    artist, { token: handler.authExternal }
+                                )
+                                return {
+                                    artistid: artist.id, name: artist.name,
+                                    profile: artist.external_urls?.spotify,
+                                    followers: artist.followers?.total,
+                                    images: artist.images, genres: artist.genres
+                                        .map((genre) => { return { name: genre, about: '' } })
+                                }
+                            }
+                        )
+                    )
+                    if (artists.status_code) { handler.data = { error: artists } }
+                    else { handler.data.artists = artists }
+                }
+            }
         }
         return await handler
     }
 
 
-    static async get(endpoint, token) {
-        const response = await fetch(
-            endpoint, { headers: { 'Authorization': `Bearer ${token}` } }
-        ).then(response => response.json())
-        return !response.error ? response :
+    static async get(endpoint, { method, body, token }) {
+        let args = new Object()
+        if (method) { args.method = method }
+        if (body) {
+            args.headers = { 'Content-Type': 'application/json' }
+            args.body = JSON.stringify(body)
+        }
+        if (token) { args.headers = { 'Authorization': `Bearer ${token}` } }
+
+
+        let response = await fetch(endpoint, args)
+
+        return response.ok ? await response.json() :
             {
                 ...{
                     400: { name: 'BadRequest' },
                     401: { name: 'Unauthorized' },
-                    404: { name: 'NotFound' }
-                }[response.error.status], status_code: response.error.status
+                    404: { name: 'NotFound' },
+                    500: { name: 'InternalServerError' }
+                }[response.status], status_code: response.status
             }
     }
 }
