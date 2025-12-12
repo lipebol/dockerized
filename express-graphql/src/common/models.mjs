dc57459bf0cc2a70ce7f655e635976c447d913d0
lipebol <lipe_bol5@hotmail.com>
2025-12-12 13:29:55 +0000

    changed models and add model: 'spotifExAlbums'

--- express-graphql/src/common/models.mjs
+++ express-graphql/src/common/models.mjs
@@ -6,7 +6,7 @@ export const spotifExGenres = mongooseSpotifEx.model(
         {
             id: { type: mongoose.ObjectId, required: true },
             name: { type: String, required: true },
-            url: { type: String, required: true }
+            about: { type: String, required: true }
         }, mongooseAddons
     ), 'genres'
 )
@@ -15,27 +15,67 @@ export const spotifExArtists = mongooseSpotifEx.model(
     'spotifExArtists', new mongoose.Schema(
         {
             id: { type: mongoose.ObjectId, required: true },
+            artistid: { type: String, required: true },
             name: { type: String, required: true },
             profile: { type: String, required: true },
+            followers: { type: Number, required: true },
+            images: [
+                {
+                    url: { type: String, required: true },
+                    width: { type: Number, required: true },
+                    height: { type: Number, required: true },
+                }
+            ],
             genres: [{ type: mongoose.ObjectId, ref: 'spotifExGenres' }]
         }, mongooseAddons
     ), 'artists'
 )
+
+
+export const spotifExAlbums = mongooseSpotifEx.model(
+    'spotifExAlbums', new mongoose.Schema(
+        {
+            id: { type: mongoose.ObjectId, required: true },
+            albumid: { type: String, required: true },
+            name: { type: String, required: true },
+            album_type: { type: String, required: true },
+            release_date: { type: String, required: true },
+            available_markets: [{ type: String, required: true }],
+            external_url: { type: String, required: true },
+            images: [
+                {
+                    url: { type: String, required: true },
+                    width: { type: Number, required: true },
+                    height: { type: Number, required: true },
+                }
+            ],
+            total_tracks: { type: Number, required: true },
+            copyrights: [
+                {
+                    text: { type: String, required: true },
+                    type: { type: String, required: true },
+                }
+            ],
+            label: { type: String, required: true }
+        }, mongooseAddons
+    ), 'albums'
+)
+
 
 export const spotifExTracks = mongooseSpotifEx.model(
     'spotifExTracks', new mongoose.Schema(
         {
             id: { type: mongoose.ObjectId, required: true },
             trackid: { type: String, required: true },
-            length: { type: Number, required: true },
-            artUrl: { type: String, required: true },
-            album: { type: String, required: true },
-            artist: { type: mongoose.ObjectId, ref: 'spotifExArtists' },
-            autoRating: { type: Number, required: true },
-            discNumber: { type: String, required: true },
-            title: { type: String, required: true },
-            trackNumber: { type: Number, required: true },
-            url: { type: String, required: true }
+            name: { type: String, required: true },
+            album: { type: mongoose.ObjectId, ref: 'spotifExAlbums' },
+            artists: [{ type: mongoose.ObjectId, ref: 'spotifExArtists' }],
+            duration_ms: { type: Number, required: true },
+            popularity: { type: Number, required: true },
+            explicit: { type: Boolean, required: true },
+            track_number: { type: Number, required: true },
+            disc_number: { type: Number, required: true },
+            isrc: { type: String, required: true }
         }, mongooseAddons
     ), 'tracks'
 )
